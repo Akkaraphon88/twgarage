b85b5cd2caf2552f537103dcd4d324fac78f9fb9
Akkaraphon88 <akkaraphonbtc@gmail.com>
2025-05-03 20:44:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ window.addEventListener('scroll', function() {
 
   // ถ้าล้อเข้ามาในหน้าจอ (เลื่อนถึง)
   if (wheelPosition <= window.innerHeight * 0.8) {
-    wheel.classList.add('is-visible');
+    wheel.classList.add('is-visible'); // เพิ่มคลาสที่ทำให้ล้อหมุน
   } else {
-    wheel.classList.remove('is-visible');
+    wheel.classList.remove('is-visible'); // ลบคลาสหากล้อไม่ได้อยู่ในหน้าจอ
   }
 });
